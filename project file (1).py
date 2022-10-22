f04b5e04fd92731a504388129acc7a54c773e867
Muralibagadi <116419006+Muralibagadi@users.noreply.github.com>
2022-10-22 17:15:02 +0530

    Add files via upload

---
+++ project file (1).py
@@ -0,0 +1,53 @@
+from main import BikeRental, Customer
+
+
+def main():
+    shop = BikeRental(100)
+    customer = Customer()
+
+    while True:
+        print("""
+        ====== Bike Rental Shop =======
+        1. Display available bikes
+        2. Request a bike on hourly basis 400rs
+        3. Request a bike on daily basis 1500rs
+        4. Request a bike on weekly basis 4000rs
+        5. Return a bike
+        6. Exit
+        """)
+
+        choice = input("Enter choice: ")
+
+        try:
+            choice = int(choice)
+        except ValueError:
+            print("Enter above numbers!")
+            continue
+
+        if choice == 1:
+            shop.displaystock()
+
+        elif choice == 2:
+            customer.rentalTime = shop.rentBikeOnHourlyBasis(customer.requestBike())
+            customer.rentalBasis = 1
+
+        elif choice == 3:
+            customer.rentalTime = shop.rentBikeOnDailyBasis(customer.requestBike())
+            customer.rentalBasis = 2
+
+        elif choice == 4:
+            customer.rentalTime = shop.rentBikeOnWeeklyBasis(customer.requestBike())
+            customer.rentalBasis = 3
+
+        elif choice == 5:
+            customer.bill = shop.returnBike(customer.returnBike())
+            customer.rentalBasis, customer.rentalTime, customer.bikes = 0, 0, 0
+        elif choice == 6:
+            break
+        else:
+            print("Invalid input. Please enter number between 1-6 ")
+    print("Thank you for using the bike rental system.")
+
+
+if __name__ == "__main__":
+    main()
